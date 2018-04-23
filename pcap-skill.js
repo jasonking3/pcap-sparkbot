7bd92e069bf5429964c16889a86c84f083d91f44
Jason King <jk1ng@yahoo.com>
2018-04-23 08:37:41 -0700

    Added new feature

--- pcap-skill.js
+++ pcap-skill.js
@@ -103,5 +103,9 @@ module.exports = function(controller) {
     controller.hears(['.*'], 'direct_message,direct_mention', function(bot, message) {
         bot.reply(message, 'I\'m sorry, I did not understand your request.  Try asking me for help using the **help** command.');
     });
+
+    controller.hears(['^foo$'], 'direct_message,direct_mention', function(bot, message) {
+        bot.reply(message, 'bar');
+    });
     
 };
